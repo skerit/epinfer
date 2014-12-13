c8e41ac8dbb1556af69b10aede4592b95c45e63d
Jelle De Loecker <jelle@kipdola.be>
2014-12-13 22:03:05 +0200

    More finetuning

--- test.js
+++ test.js
@@ -20,10 +20,32 @@ var tests = [
 	'One.Piece.E576.VOSTFR.720p.HDTV.x264-MARINE-FORD.srt',
 	'One.Piece.E576.VOSTFR.720p.HDTV.x264-MARINE-FORD.mkv',
 	'Pokémon S16 - E29 - 1280*720 HDTV VF.mkv',
-	'Friday Night Lights S01E19 - Ch-Ch-Ch-Ch-Changes.avi',
+	'The Office [401] Fun Run.avi',
+	'Series/Californication/Season 2/Californication.2x05.Vaginatown.HDTV.XviD-0TV.avi',
+	'Real.Time.With.Bill.Maher.2014.10.31.HDTV.XviD-AFG.avi',
+	'[FlexGet] Test 12',
+	'[NoobSubs] 06 Sword Art Online II (720p 8bit AAC).mp4',
+	'FooBar.7v3.PDTV-FlexGet',
+	'FooBar.7.PDTV-FlexGet',
+	'Duckman - 101 (01) - 20021107 - I, Duckman.avi',
+	'FlexGet.14.of.21.Title.Here.720p.HDTV.AAC5.1.x264-NOGRP',
 	'Dexter.5x02.Hello,.Bandit.ENG.-.sub.FR.HDTV.XviD-AlFleNi-TeaM.[tvu.org.ru].avi',
-	'The Office [401] Fun Run.avi'
+	'Doctor Who (2005) - S06E13 - The Wedding of River Song.mkv',
+	'Marvels.Agents.of.S.H.I.E.L.D.S01E06.720p.HDTV.X264-DIMENSION.mkv',
+	'Friday Night Lights S01E19 - Ch-Ch-Ch-Ch-Changes.avi',
+	'Something.1xAll-FlexGet'
 ];
+
+tests = ['The.Office.(US).1x03.Health.Care.HDTV.XviD-LOL.avi']
+
+// var yaml = require('js-yaml'),
+//     fs = require('fs');
+
+// var doc = yaml.safeLoad(fs.readFileSync('./test/episodes.yaml', 'utf8'));
+
+// console.log(doc)
+
+// return
 
 //tests = ['Doctor.Who.(2005).1x01.Rose.mp4']
 
